baa11b52e53a40268de57b6c09a48c7035507998
ftvoid <gensaicuttlefish@gmail.com>
2018-01-27 11:40:30 +0900

    DataManager実装。

---
+++ Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour {
+    /// <summary>
+    /// 残り時間(s)
+    /// </summary>
+    public float RemainTime;
+
+    /// <summary>
+    /// HP
+    /// </summary>
+    public float HP;
+}
