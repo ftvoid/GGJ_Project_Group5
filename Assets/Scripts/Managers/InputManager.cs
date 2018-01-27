921b8b3a24510450c2e62b86ae3679ad8537cdd6
ftvoid <gensaicuttlefish@gmail.com>
2018-01-27 15:44:10 +0900

    スワイプ判定実装。

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -11,9 +11,15 @@ using UniRx.Triggers;
 public class InputManager : SingletonMonoBehaviour<InputManager> {
     private Subject<Vector2> _onPress = new Subject<Vector2>();
     private Subject<Vector2> _onRelease = new Subject<Vector2>();
+    private Subject<Vector2> _onBeginSwipe = new Subject<Vector2>();
+    private Subject<Vector2> _onSwiping = new Subject<Vector2>();
+    private Subject<Vector2> _onEndSwipe = new Subject<Vector2>();
 
     public static IObservable<Vector2> OnPress => Instance._onPress;
     public static IObservable<Vector2> OnRelease => Instance._onRelease;
+    public static IObservable<Vector2> OnBeginSwipe => Instance._onBeginSwipe;
+    public static IObservable<Vector2> OnSwiping => Instance._onSwiping;
+    public static IObservable<Vector2> OnEndSwipe => Instance._onEndSwipe;
 
     public static bool IsPress => Input.GetMouseButton(0);
     public static bool IsDown => Input.GetMouseButtonDown(0);
@@ -23,12 +29,34 @@ public class InputManager : SingletonMonoBehaviour<InputManager> {
     protected override void Awake() {
         base.Awake();
 
+        // タップ判定
         this.ObserveEveryValueChanged(_ => IsPress)
             .Subscribe(x => {
                 if ( x ) {
                     _onPress.OnNext(Position);
                 } else {
                     _onRelease.OnNext(Position);
+                }
+            });
+
+        // スワイプ判定
+        bool isSwiping = false;
+        this.ObserveEveryValueChanged(_ => Position)
+            .Where(_ => IsPress)
+            .Subscribe(_ => {
+                if ( isSwiping ) {
+                    _onSwiping.OnNext(Position);
+                } else {
+                    _onBeginSwipe.OnNext(Position);
+                    isSwiping = true;
+                }
+            });
+        this.ObserveEveryValueChanged(_ => IsPress)
+            .Where(x => !x)
+            .Subscribe(_ => {
+                if ( isSwiping ) {
+                    _onEndSwipe.OnNext(Position);
+                    isSwiping = false;
                 }
             });
     }
