7865867d7e57a9c77c1d4f7308ff56ce3618cb9f
ftvoid <gensaicuttlefish@gmail.com>
2018-01-27 20:11:45 +0900

    ゲームUI実装。 DataManagerをReactivePropertyに対応。

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// ゲーム中のUI管理
+/// </summary>
+public class UIManager : SingletonMonoBehaviour<UIManager> {
+    [SerializeField]
+    private Text _remainTime;
+
+    [SerializeField]
+    private Text _hp;
+
+    public float RemainTime {
+        set {
+            if ( _remainTime == null ) {
+                return;
+            }
+            int intTime = (int)value;
+            _remainTime.text = string.Format("{0:00}:{1:00}", intTime / 60, intTime % 60);
+        }
+    }
+
+    public float HP {
+        set {
+            if ( _hp == null ) {
+                return;
+            }
+            _hp.text = string.Format("HP : {0}", value);
+        }
+    }
+}
