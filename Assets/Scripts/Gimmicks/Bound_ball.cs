ef03544709068fae88b72212e018a708158624c4
syunto <syuntosoccerlove@gmail.com>
2018-01-28 01:17:27 +0900

    <Gimmick追加>chroome_ballを追加しました。

--- Assets/Scripts/Gimmicks/Bound_ball.cs
+++ Assets/Scripts/Gimmicks/Bound_ball.cs
@@ -6,24 +6,52 @@ public class Bound_ball : MonoBehaviour
 {
     Rigidbody2D _rg;
     Transform rote;
-    float x;
-    float y;
+    private float time = 0.0f;
+    public float Damage = 50.0f;
+
+    [SerializeField]
+    [Header("分裂時間")]
+    private float timeout = 3.0f;
+
+    [SerializeField]
+    [Header("速度")]
+    private float speed = 10.0f;
 
 	void Start ()
     {
         _rg = GetComponent<Rigidbody2D>();
         transform.Rotate(new Vector3(0,0,Random.Range(180, 0)));
+        _rg.velocity = transform.up * speed;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetMouseButton(0))
+
+        time += Time.deltaTime;
+        if(time >= timeout)
         {
-            _rg.velocity = transform.up * 20;
-            //_rg.AddForce(new Vector3(2,2,0), ForceMode2D.Impulse);
+
+            Instantiate(gameObject, transform.position, transform.transform.rotation);
+            time = 0.0f;
         }
+
 	}
 
-    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            GimmickManager.Instance.SoundStart(0);
+        }
+    }
+
+    public GameObject CloneBound()
+    {
+        //transform.Rotate(new Vector3(0, 0, Random.Range(180, 0)));
+        GameObject bound_Clone = Instantiate(gameObject, transform.position, transform.transform.rotation);
+        return bound_Clone;
+    }
+
+
 }
